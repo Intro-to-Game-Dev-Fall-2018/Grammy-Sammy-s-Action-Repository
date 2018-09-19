88133ea3b260aa75a8c8911441a0a2fc8c90f4a9
Samantha Chuang <ssc538@nyu.edu>
2018-09-19 10:51:36 -0400

    Added Action Game project.

---
+++ Grammy Sammy's Action Game/Assets/Cars/Car1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Car1 : MonoBehaviour
+{
+	private Camera m_MainCamera;
+	private Vector2 startPos;
+
+	public float startX;
+	public float startY;
+	public float speed;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		m_MainCamera = Camera.main;
+		startPos = new Vector2(startX, startY);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//this is fine and dandy for now but try making it so that it reverts to the original position when
+		//it hits the edge of the screen, not just when it exceeds x = 10
+		//Debug.Log("Camera width is: " + Screen.width + " and the car is at " + transform.position.x);
+		if (transform.position.x >= 10)
+		{
+			transform.position = startPos;
+		}
+
+		else
+		{
+			transform.Translate(Vector2.right * speed);
+		}
+	}
+}
---
+++ Grammy Sammy's Action Game/Assets/Player 1 (right)/Player2Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+
+    public float speed;
+    public float startLocation;
+
+    Rigidbody2D myRigidbody;
+
+    private Vector2 startPos; 
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        //Vector2 v1 = new Vector2(0, 1)
+        //Vector2 v2 = new Vector2(0, 1, 0);
+        startPos = new Vector2(-5.07f, -4.31f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * 0.1f);
+            Debug.Log("w is pressed");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.up * -0.1f);
+            Debug.Log("down arrow pressed");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObj)
+    {
+        if (otherObj.CompareTag("Car"))
+        {
+            Debug.Log("Player has collided with a car!");
+            transform.position = startPos;
+        }
+    }
+}
